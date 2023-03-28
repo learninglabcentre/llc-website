8273370081413f0d95bcaca4b18fc4acd7e642b7
learninglabcentre <129229658+learninglabcentre@users.noreply.github.com>
2023-03-28 23:04:20 +0000

    revert mail test changes

--- mail_handler.php
+++ mail_handler.php
@@ -1,11 +1,11 @@
 <?php 
-if(isset($_GET['submit'])){
+if(isset($_POST['submit'])){
     $to = "learninglabcentre@gmail.com"; // this is your Email address
-    $from = $_GET['userEmail']; // this is the sender's Email address
-    $fullName = $_GET['fullName'];
-    $contactNumber = $_GET['contactNumber'];
+    $from = $_POST['userEmail']; // this is the sender's Email address
+    $fullName = $_POST['fullName'];
+    $contactNumber = $_POST['contactNumber'];
     $subject = "Contact Us form from website";
-    $message = $fullName. " with contact details as " . $contactNumber ." wrote the following:" . "\n\n" . $_GET['message'] ;
+    $message = $fullName. " with contact details as " . $contactNumber ." wrote the following:" . "\n\n" . $_POST['message'] ;
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
